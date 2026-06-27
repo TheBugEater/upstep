f4b9eceb718b9cd18d5ae9de59ea6b28410a9074
TheBugEater <dilhangeeth@hotmail.com>
2026-06-27 23:58:35 +0100

    Fixed Mobile

--- packages/sdk-web/src/react.tsx
+++ packages/sdk-web/src/react.tsx
@@ -183,6 +183,7 @@ export function FeedbackWidget({
 
   useEffect(() => {
     const mq = window.matchMedia("(max-width: 639px)");
+    setIsMobile(mq.matches);
     const handler = (e: MediaQueryListEvent) => setIsMobile(e.matches);
     mq.addEventListener("change", handler);
     return () => mq.removeEventListener("change", handler);
